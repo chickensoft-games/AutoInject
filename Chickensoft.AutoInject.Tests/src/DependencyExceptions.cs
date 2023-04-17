9ec7be7ab8e638cab06a70a853e101041e8cf780
Joanna May <joannamadisonmay@gmail.com>
2023-04-16 22:34:37 -0500

    fix: correct dependency resolution

--- Chickensoft.AutoInject.Tests/src/DependencyExceptions.cs
+++ Chickensoft.AutoInject.Tests/src/DependencyExceptions.cs
@@ -8,9 +8,23 @@ using System;
 /// </summary>
 public class ProviderNotFoundException : InvalidOperationException {
   /// <summary>Creates a new provider not found exception.</summary>
-  /// <param name="providerType"></param>
+  /// <param name="providerType">Provider type.</param>
   public ProviderNotFoundException(Type providerType) : base(
     $"No provider found for the following type: {providerType}" + ". " +
     "Consider specifying a fallback value in `DependOn<T>(T fallback)`."
+  ) { }
+}
+
+/// <summary>
+/// Exception thrown when a dependency is accessed before the provider has
+/// called <see cref="IProvide{T}.Provide(T)"/>.
+/// </summary>
+public class ProviderNotInitializedException : InvalidOperationException {
+  /// <summary>Creates a new provider has not provided exception.</summary>
+  /// <param name="providerType">Provider type.</param>
+  public ProviderNotInitializedException(Type providerType) : base(
+    "The provider for the following type has not called Provide() yet: " +
+    $"{providerType}" + ". Please call Provide() from the provider " +
+    "once all of its dependencies have been initialized."
   ) { }
 }

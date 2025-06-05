8c173979de4d22991753a5b4eae92db1a1ad36b3
Mark Wilson <23439518+wlsnmrk@users.noreply.github.com>
2025-06-05 15:25:00 -0400

    test: add more tests for this.Provide() fixes

--- Chickensoft.AutoInject.Analyzers.Tests/test/src/fixes/AutoInjectProvideFixProviderTest.cs
+++ Chickensoft.AutoInject.Analyzers.Tests/test/src/fixes/AutoInjectProvideFixProviderTest.cs
@@ -1,6 +1,7 @@
 namespace Chickensoft.AutoInject.Analyzers.Tests;
 
 using System.Threading.Tasks;
+using Chickensoft.AutoInject.Analyzers.Fixes;
 using Chickensoft.AutoInject.Analyzers.Utils;
 using Xunit;
 using VerifyCS = Verifiers.CSharpCodeFixVerifier<AutoInjectProvideAnalyzer, Fixes.AutoInjectProvideFixProvider>;
@@ -8,13 +9,45 @@ using VerifyCS = Verifiers.CSharpCodeFixVerifier<AutoInjectProvideAnalyzer, Fixe
 // It's nontrivial to get Introspection source generation to run during the
 // test, so we fake some of the things that would ordinarily rely on it (e.g.,
 // AutoInject types, some extension methods)
-//
-// TODO: We may need to split the code-fix provider into several separate
-// providers - unclear if it's possible to test more than one fix from a single
-// provider.
 public class AutoInjectProvideFixProviderTest {
+  #region NoDiagnostic
   [Fact]
-  public async Task ReportsMissingNotificationOverride() {
+  public async Task DoesNotOfferDiagnosticIfProvideIsCalled() {
+    var testCode = $$"""
+    using Chickensoft.Introspection;
+    using Godot;
+
+    interface IProvide<T>
+    {
+        T Value();
+    }
+
+    interface IProvider;
+
+    public static class MyNodeExtensions
+    {
+        public static void Notify(this MyNode node, int what) { }
+        public static void Provide(this MyNode node) { }
+    }
+
+    class Provision;
+
+    [Meta(typeof(IProvider))]
+    public partial class MyNode : Node, IProvide<Provision>
+    {
+        Provision IProvide<Provision>.Value() => new Provision();
+        public override void _Notification(int what) => this.Notify(what);
+        public void SomeMethod() => this.Provide();
+    }
+    """;
+
+    await VerifyCS.VerifyAnalyzerAsync(testCode);
+  }
+  #endregion NoDiagnostic
+
+  #region AddingMethods
+  [Fact]
+  public async Task FixesMissingProvideCallByAddingSetup() {
     var diagnosticID = Diagnostics
       .MissingAutoInjectProvideDescriptor
       .Id;
@@ -81,7 +114,391 @@ public class AutoInjectProvideFixProviderTest {
 
     await VerifyCS.VerifyCodeFixAsync(
       testCode.ReplaceLineEndings(),
-      fixedCode.ReplaceLineEndings()
+      fixedCode.ReplaceLineEndings(),
+      AutoInjectProvideFixProvider.GetCodeFixEquivalenceKey(
+        AutoInjectProvideFixProvider.SETUP_METHOD_NAME,
+        false
+      )
     );
   }
+
+  [Fact]
+  public async Task FixesMissingProvideCallByAddingOnReady() {
+    var diagnosticID = Diagnostics
+      .MissingAutoInjectProvideDescriptor
+      .Id;
+
+    var testCode = $$"""
+    using Chickensoft.Introspection;
+    using Godot;
+
+    interface IProvide<T>
+    {
+        T Value();
+    }
+
+    interface IProvider;
+
+    public static class MyNodeExtensions
+    {
+        public static void Notify(this MyNode node, int what) { }
+        public static void Provide(this MyNode node) { }
+    }
+
+    class Provision;
+
+    [{|{{diagnosticID}}:Meta(typeof(IProvider))|}]
+    public partial class MyNode : Node, IProvide<Provision>
+    {
+        Provision IProvide<Provision>.Value() => new Provision();
+        public override void _Notification(int what) => this.Notify(what);
+    }
+    """;
+
+    var fixedCode = $$"""
+    using Chickensoft.Introspection;
+    using Godot;
+
+    interface IProvide<T>
+    {
+        T Value();
+    }
+
+    interface IProvider;
+
+    public static class MyNodeExtensions
+    {
+        public static void Notify(this MyNode node, int what) { }
+        public static void Provide(this MyNode node) { }
+    }
+
+    class Provision;
+
+    [Meta(typeof(IProvider))]
+    public partial class MyNode : Node, IProvide<Provision>
+    {
+        Provision IProvide<Provision>.Value() => new Provision();
+        public override void _Notification(int what) => this.Notify(what);
+
+        public void OnReady()
+        {
+            // Call the this.Provide() method once your dependencies have been initialized.
+            this.Provide();
+        }
+    }
+    """;
+
+    await VerifyCS.VerifyCodeFixAsync(
+      testCode.ReplaceLineEndings(),
+      fixedCode.ReplaceLineEndings(),
+      AutoInjectProvideFixProvider.GetCodeFixEquivalenceKey(
+        AutoInjectProvideFixProvider.ONREADY_METHOD_NAME,
+        false
+      )
+    );
+  }
+
+  [Fact]
+  public async Task FixesMissingProvideCallByAddingReadyOverride() {
+    var diagnosticID = Diagnostics
+      .MissingAutoInjectProvideDescriptor
+      .Id;
+
+    var testCode = $$"""
+    using Chickensoft.Introspection;
+    using Godot;
+
+    interface IProvide<T>
+    {
+        T Value();
+    }
+
+    interface IProvider;
+
+    public static class MyNodeExtensions
+    {
+        public static void Notify(this MyNode node, int what) { }
+        public static void Provide(this MyNode node) { }
+    }
+
+    class Provision;
+
+    [{|{{diagnosticID}}:Meta(typeof(IProvider))|}]
+    public partial class MyNode : Node, IProvide<Provision>
+    {
+        Provision IProvide<Provision>.Value() => new Provision();
+        public override void _Notification(int what) => this.Notify(what);
+    }
+    """;
+
+    var fixedCode = $$"""
+    using Chickensoft.Introspection;
+    using Godot;
+
+    interface IProvide<T>
+    {
+        T Value();
+    }
+
+    interface IProvider;
+
+    public static class MyNodeExtensions
+    {
+        public static void Notify(this MyNode node, int what) { }
+        public static void Provide(this MyNode node) { }
+    }
+
+    class Provision;
+
+    [Meta(typeof(IProvider))]
+    public partial class MyNode : Node, IProvide<Provision>
+    {
+        Provision IProvide<Provision>.Value() => new Provision();
+        public override void _Notification(int what) => this.Notify(what);
+
+        public override void _Ready()
+        {
+            // Call the this.Provide() method once your dependencies have been initialized.
+            this.Provide();
+        }
+    }
+    """;
+
+    await VerifyCS.VerifyCodeFixAsync(
+      testCode.ReplaceLineEndings(),
+      fixedCode.ReplaceLineEndings(),
+      AutoInjectProvideFixProvider.GetCodeFixEquivalenceKey(
+        AutoInjectProvideFixProvider.READY_OVERRIDE_METHOD_NAME,
+        false
+      )
+    );
+  }
+  #endregion AddingMethods
+
+  #region ImprovingExistingMethods
+  [Fact]
+  public async Task FixesMissingProvideCallByImprovingExistingSetup() {
+    var diagnosticID = Diagnostics
+      .MissingAutoInjectProvideDescriptor
+      .Id;
+
+    var testCode = $$"""
+    using Chickensoft.Introspection;
+    using Godot;
+
+    interface IProvide<T>
+    {
+        T Value();
+    }
+
+    interface IProvider;
+
+    public static class MyNodeExtensions
+    {
+        public static void Notify(this MyNode node, int what) { }
+        public static void Provide(this MyNode node) { }
+    }
+
+    class Provision;
+
+    [{|{{diagnosticID}}:Meta(typeof(IProvider))|}]
+    public partial class MyNode : Node, IProvide<Provision>
+    {
+        Provision IProvide<Provision>.Value() => new Provision();
+        public override void _Notification(int what) => this.Notify(what);
+        public void Setup() { }
+    }
+    """;
+
+    var fixedCode = $$"""
+    using Chickensoft.Introspection;
+    using Godot;
+
+    interface IProvide<T>
+    {
+        T Value();
+    }
+
+    interface IProvider;
+
+    public static class MyNodeExtensions
+    {
+        public static void Notify(this MyNode node, int what) { }
+        public static void Provide(this MyNode node) { }
+    }
+
+    class Provision;
+
+    [Meta(typeof(IProvider))]
+    public partial class MyNode : Node, IProvide<Provision>
+    {
+        Provision IProvide<Provision>.Value() => new Provision();
+        public override void _Notification(int what) => this.Notify(what);
+        public void Setup()
+        {
+            this.Provide();
+        }
+    }
+    """;
+
+    await VerifyCS.VerifyCodeFixAsync(
+      testCode.ReplaceLineEndings(),
+      fixedCode.ReplaceLineEndings(),
+      AutoInjectProvideFixProvider.GetCodeFixEquivalenceKey(
+        AutoInjectProvideFixProvider.SETUP_METHOD_NAME,
+        true
+      )
+    );
+  }
+
+  [Fact]
+  public async Task FixesMissingProvideCallByImprovingExistingOnReady() {
+    var diagnosticID = Diagnostics
+      .MissingAutoInjectProvideDescriptor
+      .Id;
+
+    var testCode = $$"""
+    using Chickensoft.Introspection;
+    using Godot;
+
+    interface IProvide<T>
+    {
+        T Value();
+    }
+
+    interface IProvider;
+
+    public static class MyNodeExtensions
+    {
+        public static void Notify(this MyNode node, int what) { }
+        public static void Provide(this MyNode node) { }
+    }
+
+    class Provision;
+
+    [{|{{diagnosticID}}:Meta(typeof(IProvider))|}]
+    public partial class MyNode : Node, IProvide<Provision>
+    {
+        Provision IProvide<Provision>.Value() => new Provision();
+        public override void _Notification(int what) => this.Notify(what);
+        public void OnReady() { }
+    }
+    """;
+
+    var fixedCode = $$"""
+    using Chickensoft.Introspection;
+    using Godot;
+
+    interface IProvide<T>
+    {
+        T Value();
+    }
+
+    interface IProvider;
+
+    public static class MyNodeExtensions
+    {
+        public static void Notify(this MyNode node, int what) { }
+        public static void Provide(this MyNode node) { }
+    }
+
+    class Provision;
+
+    [Meta(typeof(IProvider))]
+    public partial class MyNode : Node, IProvide<Provision>
+    {
+        Provision IProvide<Provision>.Value() => new Provision();
+        public override void _Notification(int what) => this.Notify(what);
+        public void OnReady()
+        {
+            this.Provide();
+        }
+    }
+    """;
+
+    await VerifyCS.VerifyCodeFixAsync(
+      testCode.ReplaceLineEndings(),
+      fixedCode.ReplaceLineEndings(),
+      AutoInjectProvideFixProvider.GetCodeFixEquivalenceKey(
+        AutoInjectProvideFixProvider.ONREADY_METHOD_NAME,
+        true
+      )
+    );
+  }
+
+  [Fact]
+  public async Task FixesMissingProvideCallByImprovingExistingReadyOverride() {
+    var diagnosticID = Diagnostics
+      .MissingAutoInjectProvideDescriptor
+      .Id;
+
+    var testCode = $$"""
+    using Chickensoft.Introspection;
+    using Godot;
+
+    interface IProvide<T>
+    {
+        T Value();
+    }
+
+    interface IProvider;
+
+    public static class MyNodeExtensions
+    {
+        public static void Notify(this MyNode node, int what) { }
+        public static void Provide(this MyNode node) { }
+    }
+
+    class Provision;
+
+    [{|{{diagnosticID}}:Meta(typeof(IProvider))|}]
+    public partial class MyNode : Node, IProvide<Provision>
+    {
+        Provision IProvide<Provision>.Value() => new Provision();
+        public override void _Notification(int what) => this.Notify(what);
+        public override void _Ready() { }
+    }
+    """;
+
+    var fixedCode = $$"""
+    using Chickensoft.Introspection;
+    using Godot;
+
+    interface IProvide<T>
+    {
+        T Value();
+    }
+
+    interface IProvider;
+
+    public static class MyNodeExtensions
+    {
+        public static void Notify(this MyNode node, int what) { }
+        public static void Provide(this MyNode node) { }
+    }
+
+    class Provision;
+
+    [Meta(typeof(IProvider))]
+    public partial class MyNode : Node, IProvide<Provision>
+    {
+        Provision IProvide<Provision>.Value() => new Provision();
+        public override void _Notification(int what) => this.Notify(what);
+        public override void _Ready()
+        {
+            this.Provide();
+        }
+    }
+    """;
+
+    await VerifyCS.VerifyCodeFixAsync(
+      testCode.ReplaceLineEndings(),
+      fixedCode.ReplaceLineEndings(),
+      AutoInjectProvideFixProvider.GetCodeFixEquivalenceKey(
+        AutoInjectProvideFixProvider.READY_OVERRIDE_METHOD_NAME,
+        true
+      )
+    );
+  }
+  #endregion ImprovingExistingMethods
 }
